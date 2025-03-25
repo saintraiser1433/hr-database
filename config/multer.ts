5db49c589ba480570a57e9e6088c43fab5cd8cca
John Rey Decosta <decosta.johnrey30@gmail.com>
2025-03-25 18:29:47 +0800

    added uploaded

--- config/multer.ts
+++ config/multer.ts
@@ -12,6 +12,56 @@ const storage = multer.diskStorage({
     },
 });
 
+const resumeStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/resume/'); 
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        const ext = path.extname(file.originalname);
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+    },
+});
+
+const avatarStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/avatar/'); 
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        const ext = path.extname(file.originalname);
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+    },
+});
+
+export const avatarUpload = multer({
+    storage:avatarStorage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, 
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true); // Accept images only
+        } else {
+            cb(new Error('Only image files are allowed!'));
+        }
+    },
+});
+
+export const resumeUpload = multer({
+    storage: resumeStorage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, 
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'application/pdf') {
+            cb(null, true); // Accept only PDFs
+        } else {
+            cb(new Error('Only PDF files are allowed for resume!'));
+        }
+    },
+});
+
 export const upload = multer({
     storage,
     limits: {
@@ -25,3 +75,5 @@ export const upload = multer({
         }
     },
 });
+
+
